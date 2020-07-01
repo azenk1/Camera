adefa24d693f87e6cf2226b91d6144e622f27ee5
azenk <azenk@lincolnwindows.com>
2020-07-01 16:27:54 -0500

    Firebase added.

--- app/src/main/java/com/example/camera/MainActivity.java
+++ app/src/main/java/com/example/camera/MainActivity.java
@@ -1,22 +1,37 @@
 package com.example.camera;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Storage reference for Firebase
+        mStorageRef = FirebaseStorage.getInstance().getReference();
     }
 
     public void pictureBtnClick(View view) {
@@ -35,12 +50,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         super.onActivityResult(requestCode, resultCode, data);
-        
+
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             ImageView imageView = findViewById(R.id.imageView);
             imageView.setImageBitmap(imageBitmap);
+
+            Uri file = Uri.fromFile(new File("C:\\Users\\azenk\\Pictures\2085336.jpg"));
+
+            StorageReference picRef = mStorageRef.child("Picture\2085336.jpg");
+
+            picRef.putFile(file).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    //get URL to the uploaded content
+                    Task<Uri> downloadUrl;
+                    downloadUrl = taskSnapshot.getStorage().getDownloadUrl();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                }
+            });
+
         }
     }
 }
