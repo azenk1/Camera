b89758b4255c017f75fba18d8b58a8a271e3d652
azenk <azenk@lincolnwindows.com>
2020-07-06 15:50:19 -0500

    Take photo and save to pictures directory of target environment.

--- app/src/main/java/com/example/camera/MainActivity.java
+++ app/src/main/java/com/example/camera/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.camera;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -19,11 +24,15 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    private StorageReference mStorageRef;
+    StorageReference mStorageRef;
+    String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,11 +47,41 @@ public class MainActivity extends AppCompatActivity {
         dispatchPictureIntent();
     }
 
+    static final int REQUEST_TAKE_PHOTO = 1;
     private void dispatchPictureIntent() {
+
+        //New Intent instance. ACTION_IMAGE_CAPTURE is intent action sent as parameter
+        //to instruct camera application of target environment to capture an image and
+        //return it.
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
+        //Is a camera activity available to handle this intent?
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+            File photoFile = null;
+            try{
+                photoFile = createImageFile();
+            } catch (IOException ex)
+            {
+
+                Context context = getApplicationContext();
+                CharSequence text = "Unable to create image file. Contact Al.";
+                int duration = Toast.LENGTH_LONG;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+
+            //Was photo file created successfully?
+            if (photoFile != null)
+            {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+
+            }
         }
     }
 
@@ -57,24 +96,24 @@ public class MainActivity extends AppCompatActivity {
             ImageView imageView = findViewById(R.id.imageView);
             imageView.setImageBitmap(imageBitmap);
 
-            Uri file = Uri.fromFile(new File("C:\\Users\\azenk\\Pictures\2085336.jpg"));
-
-            StorageReference picRef = mStorageRef.child("Picture\2085336.jpg");
-
-            picRef.putFile(file).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    //get URL to the uploaded content
-                    Task<Uri> downloadUrl;
-                    downloadUrl = taskSnapshot.getStorage().getDownloadUrl();
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-
-                }
-            });
-
         }
+    }
+
+    private File createImageFile() throws IOException
+    {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_ " + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+
+        currentPhotoPath = image.getAbsolutePath();
+
+        //Return image created using String imageFileName, the .jpg suffix, and File storageDir that
+        // points to the pictures directory of the target environment.
+        return image;
     }
 }
